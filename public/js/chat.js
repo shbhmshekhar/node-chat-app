a9380c6b6aa51f47b4d589d22b884d96f225b5d6
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-08-01 15:51:45 +0530

    add functionality to send msg, broadcast when new user joines or leaves and ability to share location

---
+++ public/js/chat.js
@@ -0,0 +1,36 @@
+const socket = io();
+
+socket.on('message', (message) => {
+  console.log(message);
+});
+
+// const chatMessage = document.querySelector('#msg');
+
+document.querySelector('#chatForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const chatMessage = e.target.elements.message;
+  //   console.log('msg', chatMessage.value);
+  socket.emit('sendMessage', chatMessage.value);
+  chatMessage.value = '';
+});
+
+document.querySelector('#sendLocation').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Sharing location is not supported on this browser');
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
+});
+
+// socket.on('countUpdated', (count) => {
+//   console.log('Count updated', count);
+// });
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//   socket.emit('increment');
+// });
--- src/index.js
+++ src/index.js
@@ -12,6 +12,35 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicDirectoryPath));
 
+io.on('connection', (socket) => {
+  socket.emit('message', 'Hi There!');
+  socket.broadcast.emit('message', 'A new user has joined!!'); // notifies all user but current user
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+  socket.on('sendLocation', ({ latitude, longitude }) => {
+    console.log(`Location: ${latitude}, ${longitude}`);
+    io.emit('message', `https://google.com/maps?q=${latitude},${longitude}`);
+  });
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left');
+  });
+});
+
 server.listen(port, () => {
   console.log(`Server started at port: ${port}`);
 });
+
+/*let count = 0;
+
+io.on('connection', (socket) => {
+  console.log('New Socket Connection');
+  socket.emit('countUpdated', count);
+
+  socket.on('increment', () => {
+    count++;
+    // socket.emit('countUpdated', count); // notifies only to the current user
+    io.emit('countUpdated', count); // notifies all connected user
+  });
+});*/
