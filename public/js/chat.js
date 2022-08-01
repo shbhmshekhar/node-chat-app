b5818efe276b7b93866c5bce3ba46e1fa7309f80
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-08-01 17:42:04 +0530

    showing msgs on UI using Mustache

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,34 @@
 const socket = io();
 
+const $messageForm = document.querySelector('#chatForm');
+const $msgFormInput = $messageForm.querySelector('input');
+const $msgFormButton = $messageForm.querySelector('button');
+const $sendLocationBtn = document.querySelector('#sendLocation');
+const $messages = document.querySelector('#messages');
+
+const $mesageTemplate = document.querySelector('#message-template').innerHTML;
+
 socket.on('message', (message) => {
   console.log(message);
+  const html = Mustache.render($mesageTemplate, {
+    message,
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
 // const chatMessage = document.querySelector('#msg');
 
-document.querySelector('#chatForm').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  $msgFormButton.setAttribute('disabled', 'disabled');
+
   const chatMessage = e.target.elements.message;
   //   console.log('msg', chatMessage.value);
   socket.emit('sendMessage', chatMessage.value, (error) => {
+    $msgFormButton.removeAttribute('disabled');
+    $msgFormInput.value = '';
+    $msgFormInput.focus();
     if (error) {
       return console.log(error);
     }
@@ -19,11 +37,11 @@ document.querySelector('#chatForm').addEventListener('submit', (e) => {
   chatMessage.value = '';
 });
 
-document.querySelector('#sendLocation').addEventListener('click', () => {
+$sendLocationBtn.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Sharing location is not supported on this browser');
   }
-
+  $sendLocationBtn.setAttribute('disabled', 'disabled');
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit(
       'sendLocation',
@@ -32,6 +50,7 @@ document.querySelector('#sendLocation').addEventListener('click', () => {
         longitude: position.coords.longitude,
       },
       () => {
+        $sendLocationBtn.removeAttribute('disabled');
         console.log('Location shared sucessfully');
       }
     );
